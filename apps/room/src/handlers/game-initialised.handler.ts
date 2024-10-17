1a1a8407fb1efe7c86ccd632ffff209c7106a004
Kengy Van Hijfte <kengy.vanhijfte@happening.xyz>
2024-10-17 11:56:40 +0200

    feat: delegate message headers
    Signed-off-by: Kengy Van Hijfte <kengy.vanhijfte@happening.xyz>

--- apps/room/src/handlers/game-initialised.handler.ts
+++ apps/room/src/handlers/game-initialised.handler.ts
@@ -1,4 +1,9 @@
-import { getContentFromMessage, Message } from '@wgp/amqp';
+import {
+  getContentFromMessage,
+  getHeadersFromMessage,
+  Message,
+  MessageHeaders,
+} from '@wgp/amqp';
 import { randomUUID } from 'crypto';
 import { jwtDecode } from 'jwt-decode';
 
@@ -17,6 +22,7 @@ export default async function handleGameInitialised(
   message: Message,
 ): Promise<void> {
   const content = getContentFromMessage(message) as GameInitialisedContent;
+  const headers = getHeadersFromMessage(message);
 
   const { jwt } = content;
   const { gid: gameId } = jwtDecode(jwt) as { gid: string };
@@ -29,22 +35,33 @@ export default async function handleGameInitialised(
   const rooms = await roundRepository.getRoomsByGameId(gameId);
   if (rooms.length > 0) {
     const room = rooms[0]; // for now, we only support one room per game
-    const roomsReceivedEvent = new RoomsReceivedEvent(config.APP.SERVICE_NAME, [
-      room,
-    ]);
+    const roomsReceivedEvent = new RoomsReceivedEvent(
+      config.APP.SERVICE_NAME,
+      [room],
+      new Date(),
+      headers as MessageHeaders,
+    );
     await roomsReceivedEvent.publish(config.RABBITMQ.EXCHANGE_WO_OUT);
     return;
   }
 
   // when no room is found, we create a new room
   const room = await roundRepository.createRoom(gameId, roomId, []);
-  const roomCreatedEvent = new RoomCreatedEvent(config.APP.SERVICE_NAME, room);
+  const roomCreatedEvent = new RoomCreatedEvent(
+    config.APP.SERVICE_NAME,
+    room,
+    new Date(),
+    headers as MessageHeaders,
+  );
   // publish the room created event to wo-internal
   await roomCreatedEvent.publish(config.RABBITMQ.EXCHANGE_WO_INTERNAL);
 
   // publish the rooms received event to wo-out
-  const roomsReceivedEvent = new RoomsReceivedEvent(config.APP.SERVICE_NAME, [
-    room,
-  ]);
+  const roomsReceivedEvent = new RoomsReceivedEvent(
+    config.APP.SERVICE_NAME,
+    [room],
+    new Date(),
+    headers as MessageHeaders,
+  );
   await roomsReceivedEvent.publish(config.RABBITMQ.EXCHANGE_WO_OUT);
 }
